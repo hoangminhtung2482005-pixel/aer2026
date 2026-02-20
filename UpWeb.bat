7e7716a03579fe3341f3056d0ed40ccc08275f9a
ATG User <atg@example.com>
2026-02-21 02:15:39 +0700

    Cap nhat ket qua moi tu may tinh

---
+++ UpWeb.bat
@@ -0,0 +1,17 @@
+@echo off
+color 0A
+echo ===================================================
+echo   DANG TU DONG DAY DU LIEU LEN GITHUB...
+echo ===================================================
+
+git add .
+git commit -m "Cap nhat ket qua moi tu may tinh"
+git push origin main
+
+echo.
+echo ===================================================
+echo   THANH CONG! 
+echo   Robot Github dang "nau" data. 
+echo   Web se cap nhat sau 1-2 phut nua nhe!
+echo ===================================================
+pause
