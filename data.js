6878e665008daa56770234942a12c376dfa470b9
ATG User <atg@example.com>
2026-02-21 01:53:48 +0700

    Update AER 2026 rankings

---
+++ data.js
@@ -0,0 +1,242 @@
+const AER_DATA = [
+    {
+        "rank": 1,
+        "team": "BRU",
+        "region": "RPL",
+        "score": 1315,
+        "matches": 7,
+        "gw_gl": "20/6",
+        "win_rate": "85.7%",
+        "tier": "S"
+    },
+    {
+        "rank": 2,
+        "team": "SLX",
+        "region": "RPL",
+        "score": 1304,
+        "matches": 7,
+        "gw_gl": "20/8",
+        "win_rate": "85.7%",
+        "tier": "S"
+    },
+    {
+        "rank": 3,
+        "team": "FS",
+        "region": "RPL",
+        "score": 1293,
+        "matches": 7,
+        "gw_gl": "19/7",
+        "win_rate": "85.7%",
+        "tier": "A"
+    },
+    {
+        "rank": 4,
+        "team": "TEN",
+        "region": "RPL",
+        "score": 1246,
+        "matches": 6,
+        "gw_gl": "13/8",
+        "win_rate": "66.7%",
+        "tier": "A"
+    },
+    {
+        "rank": 5,
+        "team": "SGP",
+        "region": "AOG",
+        "score": 1200,
+        "matches": 0,
+        "gw_gl": "0/0",
+        "win_rate": "0.0%",
+        "tier": "A"
+    },
+    {
+        "rank": 6,
+        "team": "FPT",
+        "region": "AOG",
+        "score": 1200,
+        "matches": 0,
+        "gw_gl": "0/0",
+        "win_rate": "0.0%",
+        "tier": "A"
+    },
+    {
+        "rank": 7,
+        "team": "1S",
+        "region": "AOG",
+        "score": 1200,
+        "matches": 0,
+        "gw_gl": "0/0",
+        "win_rate": "0.0%",
+        "tier": "A"
+    },
+    {
+        "rank": 8,
+        "team": "BOX",
+        "region": "AOG",
+        "score": 1200,
+        "matches": 0,
+        "gw_gl": "0/0",
+        "win_rate": "0.0%",
+        "tier": "B"
+    },
+    {
+        "rank": 9,
+        "team": "SPN",
+        "region": "AOG",
+        "score": 1200,
+        "matches": 0,
+        "gw_gl": "0/0",
+        "win_rate": "0.0%",
+        "tier": "B"
+    },
+    {
+        "rank": 10,
+        "team": "FPL",
+        "region": "AOG",
+        "score": 1200,
+        "matches": 0,
+        "gw_gl": "0/0",
+        "win_rate": "0.0%",
+        "tier": "B"
+    },
+    {
+        "rank": 11,
+        "team": "TS",
+        "region": "AOG",
+        "score": 1200,
+        "matches": 0,
+        "gw_gl": "0/0",
+        "win_rate": "0.0%",
+        "tier": "B"
+    },
+    {
+        "rank": 12,
+        "team": "GAM",
+        "region": "AOG",
+        "score": 1200,
+        "matches": 0,
+        "gw_gl": "0/0",
+        "win_rate": "0.0%",
+        "tier": "B"
+    },
+    {
+        "rank": 13,
+        "team": "FW",
+        "region": "GCS",
+        "score": 1200,
+        "matches": 0,
+        "gw_gl": "0/0",
+        "win_rate": "0.0%",
+        "tier": "C"
+    },
+    {
+        "rank": 14,
+        "team": "HKA",
+        "region": "GCS",
+        "score": 1200,
+        "matches": 0,
+        "gw_gl": "0/0",
+        "win_rate": "0.0%",
+        "tier": "C"
+    },
+    {
+        "rank": 15,
+        "team": "ONE",
+        "region": "GCS",
+        "score": 1200,
+        "matches": 0,
+        "gw_gl": "0/0",
+        "win_rate": "0.0%",
+        "tier": "C"
+    },
+    {
+        "rank": 16,
+        "team": "DCG",
+        "region": "GCS",
+        "score": 1200,
+        "matches": 0,
+        "gw_gl": "0/0",
+        "win_rate": "0.0%",
+        "tier": "C"
+    },
+    {
+        "rank": 17,
+        "team": "BMG",
+        "region": "GCS",
+        "score": 1200,
+        "matches": 0,
+        "gw_gl": "0/0",
+        "win_rate": "0.0%",
+        "tier": "C"
+    },
+    {
+        "rank": 18,
+        "team": "ANK",
+        "region": "GCS",
+        "score": 1200,
+        "matches": 0,
+        "gw_gl": "0/0",
+        "win_rate": "0.0%",
+        "tier": "C"
+    },
+    {
+        "rank": 19,
+        "team": "LIT",
+        "region": "GCS",
+        "score": 1200,
+        "matches": 0,
+        "gw_gl": "0/0",
+        "win_rate": "0.0%",
+        "tier": "D"
+    },
+    {
+        "rank": 20,
+        "team": "GJC",
+        "region": "RPL",
+        "score": 1163,
+        "matches": 8,
+        "gw_gl": "11/19",
+        "win_rate": "37.5%",
+        "tier": "D"
+    },
+    {
+        "rank": 21,
+        "team": "eA",
+        "region": "RPL",
+        "score": 1157,
+        "matches": 6,
+        "gw_gl": "10/15",
+        "win_rate": "33.3%",
+        "tier": "D"
+    },
+    {
+        "rank": 22,
+        "team": "KOG",
+        "region": "RPL",
+        "score": 1117,
+        "matches": 6,
+        "gw_gl": "8/16",
+        "win_rate": "16.7%",
+        "tier": "D"
+    },
+    {
+        "rank": 23,
+        "team": "BAC",
+        "region": "RPL",
+        "score": 1107,
+        "matches": 6,
+        "gw_gl": "7/15",
+        "win_rate": "16.7%",
+        "tier": "D"
+    },
+    {
+        "rank": 24,
+        "team": "HD",
+        "region": "RPL",
+        "score": 1100,
+        "matches": 7,
+        "gw_gl": "5/19",
+        "win_rate": "14.3%",
+        "tier": "D"
+    }
+];
