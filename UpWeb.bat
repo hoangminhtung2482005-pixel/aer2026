06b557e3184b4d846e823732719e1ca012a9b364
ATG User <atg@example.com>
2026-02-21 02:20:07 +0700

    Cap nhat ket qua tu may tinh

--- UpWeb.bat
+++ UpWeb.bat
@@ -1,17 +1,20 @@
 @echo off
 color 0A
 echo ===================================================
-echo   DANG TU DONG DAY DU LIEU LEN GITHUB...
+echo   DANG LAY DU LIEU VA DAY LEN GITHUB...
 echo ===================================================
 
+:: Lệnh này sẽ tự động copy đè file từ chỗ khác mang về đây
+copy /Y "C:\Users\ASUS\Downloads\AER 2026\aer2026_tisodoidau.txt" .
+
 git add .
-git commit -m "Cap nhat ket qua moi tu may tinh"
+git commit -m "Cap nhat ket qua tu may tinh"
 git push origin main
 
 echo.
 echo ===================================================
-echo   THANH CONG! 
-echo   Robot Github dang "nau" data. 
-echo   Web se cap nhat sau 1-2 phut nua nhe!
+echo   THANH CONG TRET DE! 
+echo   Robot Github dang nhan nguyen lieu de "nau". 
+echo   F5 Web sau 1-2 phut nua nhe!
 echo ===================================================
 pause
